4056ae658f3aa3d86b2523104981bde807c41dc8
Jeremy Loulou <books@jeremyloulou.com>
2022-08-01 00:06:58 -0400

    Began adding archiving functionality.

--- orichalcum.go
+++ orichalcum.go
@@ -6,33 +6,32 @@ import (
 		  "log"
 		  "strings"
 		  "time"
+		  "math"
 		  "encoding/json"
 		  "crypto/sha512"
 )
 
 func main() {
 		  thisLog := OriLog{}// Need this no matter what
-		  var rootDir string
+		  var oriRoot string
 		  if IsOriDir(".") {
-					 rootDir = WhereIsOriDir(".")
-					 LoadLog(rootDir, &thisLog)
+					 oriRoot = WhereIsOriDir(".")
+					 LoadLog(oriRoot, &thisLog)
 		  } else {
 					 InitOriDir()
 		  }
 
-		  fmt.Println(rootDir)
+		  fmt.Println(oriRoot)
 
 		  fmt.Println("Before update  --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---")
 		  PrintFileLog(&thisLog.FileEntries)
-		  fmt.Println(rootDir)
-		  UpdateOriDir(rootDir, &thisLog)
+		  fmt.Println(oriRoot)
+		  UpdateOriDir(oriRoot, &thisLog)
 		  fmt.Println("After update  --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---")
 		  PrintFileLog(&thisLog.FileEntries)
 
-		  WriteLog(&thisLog, ".")
+		  WriteLog(&thisLog, oriRoot)
 
-		  files := RecursiveWalk(rootDir, len(rootDir))
-		  fmt.Println(files)
 		  //fmt.Println("Orichalcum.") the original, total functionality of this program's first install 
 }
 
@@ -44,9 +43,7 @@ func LoadLog(oriroot string, logHandle *OriLog) {
 }
 
 // JSON write
-func WriteLog(logHandle *OriLog, path string) {
-
-		  oriRoot := WhereIsOriDir(path)
+func WriteLog(logHandle *OriLog, oriRoot string) {
 
 		  jfile, ok := json.Marshal(*logHandle)
 		  if ok != nil {
@@ -106,16 +103,41 @@ func ContainsOriDir(path string) bool {
 
 // If there this is not a .orichalcum/ directory anywhere aboce the curent directory, init one with the current dir as ori-root.
 func InitOriDir() {
+		  // Check that the current working-dir is not part of an ori-repo.
 		  if IsOriDir(".") {
 					 fmt.Println("This is already an orichalcum directory.")
 					 return
 		  }
 
+		  // Make the hidden folder.
 		  ok := os.Mkdir(".orichalcum", 0777)// permissions are messed up 
 		  if ok != nil {
 					 fmt.Println("Somethign went wrong...")
 		  }
+		  // Make the Vault folder
+		  ok := os.Mkdir(".orichalcum/vault", 0777)
+		  if ok != nil {
+					 fmt.Println("Somethign went wrong...")
+		  }
+
+		  // Make the RED Folder
+		  ok := os.Mkdir(".orichalcum/red", 0777)// permissions are messed up 
+		  if ok != nil {
+					 fmt.Println("Somethign went wrong...")
+		  }
+
+
+		  // Create the initial log with metadata on the repo.
+		  var Initlog OriLog
+
+		  Initlog.Meta.DateInit = time.Now().Unix()
+		  Initlog.Meta.DefaultVaultDir = ".orichalcum/vault/" //TODO config file
+
+		  // Write the initial log
+		  WriteLog(&Initlog, ".")
+
 }
+
 
 // Recursive walk returning every file under the ori-root in pairs.
 // Both the path relative to the working dir and the absolute path from the working dir "."
@@ -171,9 +193,35 @@ func UpdateOriDir(dir string, logHandle *OriLog) {
 		  }
 }
 
+
+
+
+
 // create/update file entries
 // The path is the actual path from the working directory to the file
 // the absPath is the absolute path from the oriroot to the file, the one used as a name in the log
+
+
+func IsTracked(absolutePath string, ori *OriLog) int {
+		  length := len(ori.FileEntries)
+
+		  for i := 0; i < length; i++ {
+					 if ori.FileEntries[i].Path == absolutePath {
+								return i
+					 }
+		  }
+
+		  return -1
+}
+
+func PrintTrackedFiles(ori *OriLog) {
+		  length := len(ori.FileEntries)
+
+		  for i := 0; i < length; i++ {
+					 fmt.Println(oir.FileEntries[i].Path)
+		  }
+}
+
 func TrackFile(fSlice *[]OriFile, path PathPair) {//TODO error
 		  newEntry := OriFile{
 								Path: path.absPath,
@@ -186,9 +234,12 @@ func TrackFile(fSlice *[]OriFile, path PathPair) {//TODO error
 }
 
 func UpdateTrackedFile(entry *OriFile, path PathPair) {
-		  if entry.Hash != HashFile(path.path) { 
+		  if entry.Hash != HashFile(path.path) {
+					 oldSize := entry.Size
+					 oldEdit := entry.DateMod
 					 entry.DateMod = time.Now().Unix()
 					 entry.Hash = HashFile(path.path)
+					 entry.Size = FileSize(path.path) // TODO When i implement arc, this has to be updated later, as the size is important to size chance arhcive 
 		  }
 		  /*
 		  if *entry.IsArc == true {
@@ -240,12 +291,11 @@ type OriLog struct {
 type OriMeta struct {
 		  DateInit int64
 		  DateMod int64
-		  DefualtVaultPath string
+		  DefaultVaultPath string // Relative to the oriRoot
 		  Type OriKind
 		  ID string // Identifes, a repo. This ID should be the same for a backup of the repo. 
 
 		  secret string// Key which identifies an ori-repo. When syncing orichalcum will check for matching repos with a pulbic key and use this secret to decrypt files sent
-
 }
 
 type OriKind int // The kind of orichalcum dir.
@@ -257,17 +307,19 @@ const (
 
 // Infomation about a given file
 type OriFile struct {
-		  Path string // path relative to the root of the ori directory
-		  Hash [sha512.Size]byte
+		  Path string // Path relative to the root of the ori directory
+		  Hash [sha512.Size]byte // SHA512 hash sum
 		  DateMod int64
-
 		  DateCreated int64
+		  Size int64 // Size of the file in bytes
 
 		  IsArc bool // file has auto archive option enables
 		  Mode ArcMode
-		  IsRED bool // the archived copies are saved as Redundate Error-protected Digital copies
+		  IsRED bool // A single copy is saved in the vault upon each file change which is RED protected. Originally: the archived copies are saved as Redundate Error-protected Digital copies
+		  REDFactor uint //How heavily is the files bits redundacified? (Factors of 8. I.e. if the factor is 10, the file is 80x the size)
 		  SizeChangeThresh uint // every time the file is changed a copy is saved to a vault
-		  VaultDirs []string
+		  InteriorVaultDir string // Directory within the ori-repo which hosts the file's archives.
+		  ExteriorVaultDirs []string // Directories outside of the ori-repo where backups are stored.
 }
 
 // Informaiton about an archive. Used for setting the archive option on an OriFile entry.
@@ -282,12 +334,19 @@ type OriArc struct {
 type ArcMode int
 const (
 		  None ArcMode = iota
-		  Total 
+		  Total // Every time the file is changed a copy is saved to the vault
 		  Daily
 		  Weekly
 		  Montly
 		  SizeChange// for a size change threshold - Size change is in both directions.
 		  Manual
+)
+
+type UnixPeriod int64
+const (
+		  Day UnixPeriod = 86400 // Days should behave based on date. One backup per calender day.
+		  Week UnixPeriod = 604800
+		  Month UnixPeriod = 2628000
 )
 
 func SetArc(entry *OriFile, settings *OriArc) {
@@ -311,6 +370,10 @@ func OpenFile(filename string) []byte{
 		  return f 
 }
 
+func FileSize(filename string) int64 {
+		  return os.Stat(filename).Size() 
+}
+
 // for a filepath, retrun the byte array of the hsha512 sum
 func HashFile(filename string) [sha512.Size]byte{
 
@@ -324,12 +387,14 @@ func HashFile(filename string) [sha512.Size]byte{
 		  return hash 
 }
 
-// Meta information
+// Meta information update on tracked file update.
 func OnUpdate(logHandle *OriLog) {
 		  logHandle.Meta.DateMod = time.Now().Unix()
 }
 
 // UI
+
+// Print the information tracked about one file.
 func PrintFileEntry(handle *OriFile) {
 		  fmt.Println(handle.Path)
 		  dateCreate := time.Unix(handle.DateCreated, 0)
@@ -341,11 +406,12 @@ func PrintFileEntry(handle *OriFile) {
 
 func PrintFileLog(handle *[]OriFile) {
 		  length := len(*handle)
-
 		  for i := 0; i < length; i++ {
 					 PrintFileEntry(&(*handle)[i])
 		  }
 }
+
+//Find index of last occurence of a character in a string(for finding the name of the file without the path)
 
 // Better would be slices lib. Developing withou network connection.
 func Contains[E comparable](slice []E, elem E) bool {
@@ -358,4 +424,105 @@ func Contains[E comparable](slice []E, elem E) bool {
 		  }
 
 		  return false
+}
+
+func PathpairContains(slice []Pathpair, elem string, path int) bool {
+		  length := len(slice)
+
+		  if path == 1 {// abspath
+					 for i := 0; i < length; i++ {
+								if slice[i].asbPath == elem {
+										  return true
+								}
+					 }
+		  } else if path == 2 {
+					 for i := 0; i < length; i++ {
+								if slice[i].path == elem {
+										  return true
+								}
+					 }
+
+		  }
+
+		  return false
+}
+
+
+func LastIndex(name string, elem string) int {
+		  // SHould not have any utf8 problems as, for now, we're woking with a unix filesystem
+		  for i, sub := range name {
+					 if sub == elem {
+								return i
+					 }
+		  }
+		  return -1
+}
+
+// Archiving
+
+func WriteArchiveCopy(entry *OriFile)// naimg will automate to ad _yyyymmdd_hhmmss, that could actually, temporarily serve to assure it's not overwiting another copy
+
+func ArchiveTracked(entry *OriFile, oldSize int64, previousEdit int64) {
+		  switch entry.Mode {
+		  
+					 case None: return
+
+					 case Daily: {
+		  }
+
+					 case SizeChange: {
+								if math.Abs(entry.Size - oldSize) >= entry.SizeChangeTresh {
+										  WriteArchiveCopy(entry)
+								}
+					 }
+
+
+		  }
+		  
+}
+
+// Takes an absolute path... not ideal
+func SetArch(file string, oriroot string, ori *OriLog) {
+		  index := !IsTracked(file)
+		  var answer string
+		  if index == -1 {
+					 fmt.Println("This file is not tracked! Would you like to see a list of tracked files? y/N")
+					 fmt.Scanln(&answer)
+					 answer = strings.ToLower(answer)
+					 if  answer == "y" || answer == "yes" {
+								PrintTrackedFiles(ori)
+					 }
+					 return
+		  }
+
+		  tracked := &(ori.FileEntries[i])
+
+		  // Confirm action
+
+		  // Sets the mode
+		  clear := false
+		  for !clear {
+					 fmt.Println("Which mode? (hint: 0 = None, 1 = Total, 2 = Daily, 3 = Weekly, 4 = Montly, 5 = SizeChange, 6= Manual )")
+					 var numAns int
+					 fmt.Scanln(&numAns)
+
+					 if numAns >= 6 && numAns <= 0 {
+								clear = true
+								continue
+					 }
+
+					 fmt.Println("That is not a valid mode.")
+
+		  }
+		  tracked.ArcMode = ArcMode(numAns)
+		  if numAns == 6 {
+					 fmt.Println("Please set the size change threshold (bytes):")
+					 fmt.Scanln(&numAns)
+					 tracked.SizeChangeThresh = numAns
+		  }
+
+		  // Sets the internal vault
+		  tracked.InteriorVaultDir = ori.Meta.DefaultVaultDir
+
+		  // Sets external vaults
 }
